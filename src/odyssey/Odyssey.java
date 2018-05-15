5d100269eaea77c40f79aff0c272c218811e4e5d
Jose Carlos Nuñez Valverde <josek21nunez@gmail.com>
2018-05-15 14:50:29 -0600

    JavaDoc
    Documentación del codigo

--- src/odyssey/Odyssey.java
+++ src/odyssey/Odyssey.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package odyssey;
 
 /**
@@ -11,8 +6,11 @@ package odyssey;
  */
 public class Odyssey {
 
-    /**
+    /** This method is mine
+     * 
+     * 
      * @param args the command line arguments
+     * 
      */
     public static void main(String[] args) {
         // TODO code application logic here
